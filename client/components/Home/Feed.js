13879b1d078fb29a4c67cd63cba2cecd911b6ebf
Julius170 <juliusyoola5@gmail.com>
2022-06-02 10:53:07 +0100

    adding the main page

---
+++ client/components/Home/Feed.js
@@ -0,0 +1,43 @@
+import { BsStars } from 'react-icons/bs'
+import React from 'react'
+import TweetBox from "./TweetBox"
+
+const style = {
+  wrapper: `flex-[2] border-r border-l border-[#38444d] overflow-y-scroll`,
+  header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
+  headerTitle: `text-xl font-bold`,
+}
+
+
+export default function Feed() {
+return (
+    <div className={`${style.wrapper} no-scrollbar`}>
+      <div className={style.header}>
+        <div className={style.headerTitle}>Home</div>
+        <BsStars />
+      </div>
+      <TweetBox />
+      {/* {tweets.map((tweet: Tweet, index: number) => (
+        <Post
+          key={index}
+          displayName={
+            tweet.author.name === 'Unnamed'
+              ? `${tweet.author.walletAddress.slice(
+                  0,
+                  4,
+                )}...${tweet.author.walletAddress.slice(41)}`
+              : tweet.author.name
+          }
+          userName={`${tweet.author.walletAddress.slice(
+            0,
+            4,
+          )}...${tweet.author.walletAddress.slice(41)}`}
+          text={tweet.tweet}
+          avatar={tweet.author.profileImage}
+          isProfileImageNft={tweet.author.isProfileImageNft}
+          timestamp={tweet.timestamp}
+        />
+      ))} */}
+    </div>
+  )
+}
